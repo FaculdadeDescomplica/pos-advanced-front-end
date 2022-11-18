70b3b6796ed225d9ba3e4e2f9cbb1d94b2b5db82
Bruno Hauck <brunohauck@Brunos-MacBook-Air.local>
2022-11-18 20:40:07 -0300

    Mod1video06

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  @Input() nome:string = '';
   ngOnInit(): void {
   }
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  title = 'descomplica';
   constructor() { }
-
-
-  
+  nome:string = "Professor Bruno Hauck";
   ngOnInit(): void {
   }
 
